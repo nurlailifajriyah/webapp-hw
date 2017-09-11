fb2c37de2baed3bc617ee792f958a5cf6bf6ad51
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-11 14:19:21 -0400

    Add a url pattern for calculator to urls.py

--- homework/2/webapps/urls.py
+++ homework/2/webapps/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import calculator.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^calculator/', calculator.views.calculate),
 ]
