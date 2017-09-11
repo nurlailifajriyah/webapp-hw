23eaee1c224114f3316372c633622cd5f060700f
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-11 14:13:33 -0400

    Add a function to views.py to render the calculator template

--- homework/2/calculator/views.py
+++ homework/2/calculator/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def calculate(request):
+    # Creates a Python dictionary that will be used to make name-value pairs available to the view
+    context = {}
+    context['number_value'] = ''
+
+    # Retrieves then name from the request if the 'username' HTTP GET parameter is present.
+    if 'number' in request.GET:
+        context['number_value'] = request.GET['number']
+
+    return render(request, 'calculator.html', context)
