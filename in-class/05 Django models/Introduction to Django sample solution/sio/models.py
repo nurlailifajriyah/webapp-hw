941f454eafcb68920d8b1c148e4346c88cb01382
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-13 12:21:19 -0400

    Add Student and Course classes to model.py

--- in-class/05 Django models/Introduction to Django sample solution/sio/models.py
+++ in-class/05 Django models/Introduction to Django sample solution/sio/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    andrewid = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.andrewid
+
+class Course(models.Model):
+    coursenumber = models.CharField(max_length=20)
+    coursename = models.CharField(max_length=50)
+    instructor = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.coursenumber
