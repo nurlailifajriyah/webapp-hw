d7a7a6003dea33c307b4fdbe1aa3608e602bfa27
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-14 21:24:31 -0400

    Implement main calculator function: addition, substration, multiplication, and division. Haven't checked unusual test cases.

--- homework/2/calculator/views.py
+++ homework/2/calculator/views.py
@@ -1,13 +1,57 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def calculate(request):
     # Creates a Python dictionary that will be used to make name-value pairs available to the view
     context = {}
-    context['number_value'] = ''
-
+    context['result'] = 0
+    context['operator'] = ''
+    context['num1'] = 0
+    context['num2'] = 0
     # Retrieves then name from the request if the 'username' HTTP GET parameter is present.
-    if 'number' in request.GET:
-        context['number_value'] = request.GET['number']
-
+    if 'operator' in request.GET and request.GET['operator'] != '':
+        context['operator'] = request.GET['operator']
+    if 'num1' in request.GET and request.GET['num1'] != '':
+        context['num1'] = int(request.GET['num1'])
+    if 'num2' in request.GET and request.GET['num2'] != '':
+        context['num2'] = int(request.GET['num2'])
+    if 'operatorinput' in request.GET and request.GET['operatorinput'] != '':
+        if context['num2'] != 0:
+            if context['operator'] == '+':
+                context['result'] = context['num1'] + context['num2']
+            elif context['operator'] == '-':
+                context['result'] = context['num1'] - context['num2']
+            elif context['operator'] == 'x':
+                context['result'] = context['num1'] * context['num2']
+            elif context['operator'] == '/':
+                context['result'] = context['num1'] / context['num2']
+            if request.GET['operatorinput'] == "=":
+                context['num1'] = 0
+                context['num2'] = 0
+                context['operator'] = ''
+            else:
+                context['num1'] = context['result']
+                context['num2'] = 0
+                context['operator'] = request.GET['operatorinput']
+        else:
+            context['result'] = context['num1']
+            context['num2'] = 0
+            if request.GET['operatorinput'] == "=":
+                context['num1'] = 0
+                context['operator'] = ''
+            else:
+                context['operator'] = request.GET['operatorinput']
+    elif 'numberinput' in request.GET:
+        if context['num2']:
+            context['num2'] = int(request.GET['numberinput'])
+            context['result'] = context['num2']
+        elif context['num1']:
+            if context['operator'] != '':
+                context['num2'] = request.GET['numberinput']
+                context['result'] = context['num2']
+            else:
+                context['num1'] = int(request.GET['numberinput'])
+                context['result'] = context['num1']
+        else:
+            context['num1'] = int(request.GET['numberinput'])
+            context['result'] = context['num1']
     return render(request, 'calculator.html', context)
