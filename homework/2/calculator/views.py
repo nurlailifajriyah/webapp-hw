11f3d1995084d2b308d5dc14d3ad843af471d275
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-15 21:57:05 -0400

    Add initial value on result screen and fix divided by zero case

--- homework/2/calculator/views.py
+++ homework/2/calculator/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 def calculate(request):
     # Creates a Python dictionary that will be used to make name-value pairs available to the view
     context = {}
-    context['result'] = 0
+    context['result'] = ''
     context['operator'] = ''
-    context['num1'] = 0
-    context['num2'] = 0
+    context['num1'] = ''
+    context['num2'] = ''
     # Retrieves then name from the request if the 'username' HTTP GET parameter is present.
     if 'operator' in request.GET and request.GET['operator'] != '':
         context['operator'] = request.GET['operator']
@@ -15,7 +15,7 @@ def calculate(request):
     if 'num2' in request.GET and request.GET['num2'] != '':
         context['num2'] = int(request.GET['num2'])
     if 'operatorinput' in request.GET and request.GET['operatorinput'] != '':
-        if context['num2'] != 0:
+        if context['num2'] != '':
             if context['operator'] == '+':
                 context['result'] = context['num1'] + context['num2']
             elif context['operator'] == '-':
@@ -23,20 +23,25 @@ def calculate(request):
             elif context['operator'] == 'x':
                 context['result'] = context['num1'] * context['num2']
             elif context['operator'] == '/':
-                context['result'] = context['num1'] / context['num2']
-            if request.GET['operatorinput'] == "=":
-                context['num1'] = 0
-                context['num2'] = 0
+                if context['num2'] == 0:
+                    context['result'] = "error"
+                else:
+                    context['result'] = context['num1'] / context['num2']
+            if context['result'] == "error":
+                context['num2'] = ''
+            elif request.GET['operatorinput'] == "=":
+                context['num1'] = context['result']
+                context['num2'] = ''
                 context['operator'] = ''
             else:
                 context['num1'] = context['result']
-                context['num2'] = 0
+                context['num2'] = ''
                 context['operator'] = request.GET['operatorinput']
         else:
             context['result'] = context['num1']
-            context['num2'] = 0
+            context['num2'] = ''
             if request.GET['operatorinput'] == "=":
-                context['num1'] = 0
+                context['num1'] = ''
                 context['operator'] = ''
             else:
                 context['operator'] = request.GET['operatorinput']
