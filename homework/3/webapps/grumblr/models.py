cb8db1bca5654f71030b52b9d90814209c1ea401
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-18 23:40:31 -0400

    Fix model and db.sqlite3

--- homework/3/webapps/grumblr/models.py
+++ homework/3/webapps/grumblr/models.py
@@ -2,18 +2,12 @@
 from django.db import models
 from django.utils import timezone
 
-# Data model for a todo-list item
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=100)
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     blog_text = models.TextField()
     published_time = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User)
-
 
     def __unicode__(self):
         return self.blog_text
