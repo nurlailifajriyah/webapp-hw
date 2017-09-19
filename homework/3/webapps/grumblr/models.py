5fc5697b9208c0076a81bb5291dbe3cf3397074c
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-18 22:39:45 -0400

    Add model and db.sqlite3

--- homework/3/webapps/grumblr/models.py
+++ homework/3/webapps/grumblr/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Data model for a todo-list item
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+
+class BlogPost(models.Model):
+    blog_text = models.TextField()
+    published_time = models.DateTimeField(default=timezone.now)
+    user_id = models.ForeignKey(User)
+
+
+    def __unicode__(self):
+        return self.blog_text
