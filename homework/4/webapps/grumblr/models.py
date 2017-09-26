6283e1fd6c667aa14e086977fab498cf339f09e2
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-09-25 21:27:07 -0400

    Add new model for user's additional info

--- homework/4/webapps/grumblr/models.py
+++ homework/4/webapps/grumblr/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 from django.contrib.auth.models import User
+class UserInfo(models.Model):
+    age = models.IntegerField()
+    short_bio = models.TextField(max_length=420)
 
 class BlogPost(models.Model):
-    blog_text = models.TextField()
+    blog_text = models.TextField(max_length=42)
     published_time = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User)
 
