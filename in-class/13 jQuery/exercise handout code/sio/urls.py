42e1b8aba4bf491d82f7b0e3ab2df4d91cf607e6
Charlie Garrod <charlie@cs.cmu.edu>
2017-10-11 08:52:55 -0400

    Staff add in-class directory 13 jQuery

---
+++ in-class/13 jQuery/exercise handout code/sio/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^home$', views.home),
+    url(r'^get-courses$', views.get_courses),
+    url(r'^create-student$', views.create_student),
+    url(r'^create-course$', views.create_course),
+    url(r'^register-student$', views.register_student),
+]
