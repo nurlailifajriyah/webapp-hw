d2c1984c32ab0119fc6d2e26a1e05dd72c1f67d9
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-11-01 12:29:32 -0400

    Unit testing in Django

--- in-class/19 Unit testing/Testing exercise/sio/tests.py
+++ in-class/19 Unit testing/Testing exercise/sio/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from sio.models import *
 
-# Create your tests here.
+# /sio/create-student (andrew_id, first_name, last_name)
+
+class CreateStudentModelsTest(TestCase):
+    fixtures = ['sample-data']
+
+    def test_add_student(self):    # Tests the to-do list add-item function.
+        self.assertTrue(Student.objects.all().count() == 0)
+        new_item = Student(andrew_id='nfajriya', first_name='nurlaili', last_name='fajriyah')
+        new_item.save()
+        self.assertTrue(Student.objects.all().count() == 1)
+
+class SioTest(TestCase):
+    def test_create_student(self):
+        client = Client()       # add-item expects a POST request with one
+                                # query parameter, item, the text of the to-do
+                                # list item.
+        sample_andrew = 'nfajriya'
+        sample_fn = 'nurlaili'
+        sample_ln = 'fajriyah'
+
+        response = client.post('/sio/create-student', {'andrew_id':sample_andrew})
+        self.assertTrue(response.content.find(sample_student.encode()) >= 0)
